1212839d2b2d5ae6b41bcb1fc9f3009144a1e533
pedro rivera <ppsirg@gmail.com>
2018-12-06 18:36:04 -0500

    improved data

--- js/board.js
+++ js/board.js
@@ -1,10 +1,5 @@
 Vue.component('ecard', {
   delimiters: ['[[', ']]'],
-  data: function () {
-    return {
-      count: 0
-    }
-  },
   props: [
     'txt', 'bg', 'sets', 'img'
   ],
@@ -35,6 +30,39 @@ Vue.component('dpicker', {
    <option v-for="item in opt" v-bind:value="item">[[item]]</option>
   </select>
   </div> `
+})
+
+Vue.component('ppanel', {
+  delimiters: ['[[', ']]'],
+  data: function () {
+    return {
+      routine: {
+          {'tag': 'lunes-viernes', 'data':[],}
+          {'tag': 'martes-jueves', 'data':[],}
+          {'tag': 'miercoles-sabado', 'data':[],}
+      }
+    }
+  },
+  template:`
+  <div>
+  <div class="pure-menu pure-menu-horizontal">
+      <div class="pure-menu-heading">gymboree</div>
+      <dpicker options="lunes-viernes martes-jueves miercoles-sabado"></dpicker>
+  </div>
+  <div class="l-content" >
+      <div class="pricing-tables pure-g" v-for="tags in steps">
+          <div class="pure-u-1 pure-u-md-1-3" v-for="item in tags">
+              <ecard
+              v-bind:txt='item.txt'
+              v-bind:bg='item.bg'
+              v-bind:img='item.img'
+              v-bind:sets='item.sets'
+              ></ecard>
+          </div>
+      </div>
+  </div>
+  </div>
+  `
 })
 
 // lunes-viernes
